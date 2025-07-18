6864760981e5d50ca9fc3b85f78613eca020d229
Luis de Andrade <quintasluis75@gmail.com>
2025-07-18 21:12:50 +0100

    Update Sintoma.java
    Signed-off-by: Luis de Andrade <quintasluis75@gmail.com>

--- Sintoma.java
+++ Sintoma.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 public class Sintoma {
     private String descricao;
     private LocalDate data;
-    private int gravidade;  // escala de 1 a 10
+    private int gravidade;  
 
     public Sintoma(String descricao, LocalDate data, int gravidade) {
         this.descricao = descricao;
@@ -11,7 +11,6 @@ public class Sintoma {
         this.gravidade = gravidade;
     }
 
-    // Getters
 
     public String getDescricao() {
         return descricao;
